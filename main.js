729c3f9f27e4842afd0b63ebeb55d90f659ced8b
Gurteg Singh <gurtegsohi123@gmail.com>
2025-06-05 17:37:00 -0400

    working prototype made

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const { GoogleGenAI } = require("@google/genai");
 require('dotenv').config();
+const readlineSync = require('readline-sync');
 
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-
+let prompt ="";
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "Explain how AI works in a few words",
+    contents: prompt,
   });
-  console.log(response.text);
+  return response.text;
 }
 
-main();
+// main();
+async function chatting(){
+    prompt = readlineSync.question('How can I help you today ?');
+
+    const response = await main();
+    return response;
+}
+
+async function run(){
+  const b = await chatting();
+  console.log(b);
+}
+
+run();
