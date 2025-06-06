18c48e47ca9bd7f7ecd1c41420778c6445937019
Gurteg Singh <gurtegsohi123@gmail.com>
2025-06-05 21:24:59 -0400

    weather API added

--- main.js
+++ main.js
@@ -15,8 +15,15 @@ async function main() {
 
 // main();
 async function chatting(){
-    prompt = readlineSync.question('How can I help you today ?');
-
+    const question = readlineSync.question('How can I help you today ?');
+    prompt = `HI , The user has asked for this information : ${question} . Can u tell whether user is asking about weather of some place or not. 
+    Create a JSON like this one :
+    {
+      llmtask : true,
+      location : london
+    } if user is asking about weather of a place set llmtask as true and in location mention the place whose weather user wants to know
+     ... if user is not asking weather of a place simply return lltask:false in JSON nothing else..   
+     Listen your output should start with curly braces of JSON and end with that ... no extra stuff u will provide .. just a JSON`
     const response = await main();
     return response;
 }
