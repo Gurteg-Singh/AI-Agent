35d6dd4ee256601aeae3d3047a4ce00c15afd32b
Gurteg Singh <gurtegsohi123@gmail.com>
2025-06-05 22:43:36 -0400

    Weather services added in LLM model

--- main.js
+++ main.js
@@ -15,22 +15,43 @@ async function main() {
 
 // main();
 async function chatting(){
-    const question = readlineSync.question('How can I help you today ?');
+    const question = readlineSync.question('What is in your mind ?');
     prompt = `HI , The user has asked for this information : ${question} . Can u tell whether user is asking about weather of some place or not. 
     Create a JSON like this one :
     {
       llmtask : true,
       location : london
-    } if user is asking about weather of a place set llmtask as true and in location mention the place whose weather user wants to know
-     ... if user is not asking weather of a place simply return lltask:false in JSON nothing else..   
+    } if user is asking about weather of a place set llmtask as false and in location mention the place whose weather user wants to know
+     ... if user is not asking weather of a place simply return lltask:true in JSON nothing else..   
      Listen your output should start with curly braces of JSON and end with that ... no extra stuff u will provide .. just a JSON`
-    const response = await main();
-    return response;
+
+    let response = await main();
+    response = response.trim();
+    response = response.replace(/^```json\s*|```$/g,'').trim();
+    const data = JSON.parse(response);
+
+
+    if(data.llmtask === true){
+        prompt = question;
+    }else{
+      const weatherinfo  = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API}&q=${data.location}&aqi=no`);
+      const result =  await weatherinfo.json();
+      const input = JSON.stringify(result);
+
+      prompt = `Hey here is weather info i fetched for you : ${input} . Now your task is to read this info and give a short summary of weather of that place.`
+    }
+    const ans = await main();
+    return ans;
 }
 
 async function run(){
-  const b = await chatting();
-  console.log(b);
+  let  i =0;
+  while(i<3){
+    const b = await chatting();
+    console.log(b);
+    i++;
+  }
+  console.log("Your session has expired");
 }
 
 run();
